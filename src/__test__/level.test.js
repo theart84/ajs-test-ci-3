4054ab919bc809ae6176c4e9b16ca45cee762094
theart84 <artshadow74@gmail.com>
2021-02-11 19:19:30 +0500

    Task completed

---
+++ src/__test__/level.test.js
@@ -0,0 +1,24 @@
+import getLevel from '../js/level';
+import fetchData from '../js/http';
+
+jest.mock('../src/http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('Проверяем если статус ok', () => {
+  fetchData.mockReturnValue({
+    status: 'ok',
+    level: 10,
+  });
+  expect(getLevel(1)).toBe('Ваш текущий уровень: 10');
+});
+
+test('Проверяем если статус не ok', () => {
+  fetchData.mockReturnValue({
+    status: 'error',
+    level: 10,
+  });
+  expect(getLevel(1)).toBe('Информация об уровне временно недоступна');
+});
